fe87bf7bb1cbf03f0a5efbd4fa0cbe2f6af55a82
Dusticcus <dasafady@gmail.com>
2022-08-10 01:18:03 -0500

    merged new front end with new back end within this branch

--- public/js/homepagemusic.js
+++ public/js/homepagemusic.js
@@ -1,3 +1,5 @@
+var base_url = window.location.origin;
+
 var audioLanding = new Audio('./images/iwasjoking.mp3');
 
 let landingPageVolume = document.getElementById('formControlRange');
@@ -13,25 +15,25 @@ setInterval(function () {
 }, 1000);
 
 const startGame = document.getElementById('startGame');
-const gameSite = 'http://localhost:3001/game'
+const gameSite = '/game'
 startGame.addEventListener('click', function() {
-    window.location = gameSite;
+    window.location = base_url + gameSite;
 })
 
 const howtoplay = document.getElementById('howtoplay');
-const howtoplaySite = 'http://localhost:3001/howtoplay'
+const howtoplaySite = '/howtoplay'
 howtoplay.addEventListener('click', function() {
-    window.location = howtoplaySite;
+    window.location = base_url + howtoplaySite;
 })
 
 const viewHighscore = document.getElementById('viewHighscore');
-const viewHighscoreSite = 'http://localhost:3001/api/userscorepage'
+const viewHighscoreSite = '/api/userscorepage'
 viewHighscore.addEventListener('click', function() {
-    window.location = viewHighscoreSite;
+    window.location = base_url + viewHighscoreSite;
 })
 
 const title = document.getElementById('title');
-const titleSite = 'http://localhost:3001/#'
+const titleSite = base_url;
 title.addEventListener('click', function() {
-    window.location = titleSite;
+    window.location = base_url + titleSite;
 })
