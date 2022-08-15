b220e5760b98b504f0ed0182f29ea348ac782d9a
Dusticcus <dasafady@gmail.com>
2022-08-15 03:10:35 -0500

    non signed in user can leave score now as player two

--- controllers/api/highscoresRoutes.js
+++ controllers/api/highscoresRoutes.js
@@ -9,47 +9,55 @@ const { QueryTypes } = require('sequelize');
 
 // --------Get Highscore Data-------------
 router.get('/', async (req, res) => {
-    try {
-      const scoreData = await Highscore.findAll({
-        attributes: { exclude: ['password'] },
-        include: [
-          {
-            model: User,
-            attributes: ['username'],
-          },
-        ],
-        // order: [['username', 'ASC']],
-      });
-      const userScores = scoreData.map((userScore) =>
+  try {
+    const scoreData = await Highscore.findAll({
+      attributes: { exclude: ['password'] },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+      // order: [['username', 'ASC']],
+    });
+    const userScores = scoreData.map((userScore) =>
       userScore.get({ plain: true })
-      
+
     );
     console.log(userScores)
     res.render('userscorespage', {
       userScores,
     });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-  // route to create/add a SCORE using async/await
+// route to create/add a SCORE using async/await
 router.post('/', async (req, res) => {
-  try { 
+  let user_id;
+  if (!req.session) {
+    user_id = req.session.user_id;
+    
+  } else {
+    user_id = 1;
+  }
+
+  try {
     const scoreData = await Highscore.create({
-    score: req.body.score,
-    user_id: req.session.user_id,
-  });
-  
-  // if the SCORE is successfully created, the new response will be returned as json
-  res.status(200).json(scoreData)
-  console.log(scoreData)
-  // res.render('userscorespage', {
-  //   userScores,
-  // });
-} catch (err) {
-  res.status(400).json(err);
-}
+      score: req.body.score,
+      user_id: user_id,
+    });
+
+    // if the SCORE is successfully created, the new response will be returned as json
+    res.status(200).json(scoreData)
+    console.log(scoreData)
+    // res.render('userscorespage', {
+    //   userScores,
+    // });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // ------------------------------
